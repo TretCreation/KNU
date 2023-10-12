139732eb55c858537303b13cb261257d32a4307a
Volodymyr <tret.creation@gmail.com>
2023-10-12 16:01:42 +0300

    upd: lab1 [MDP]

--- Multithreaded-and-distributed-programming/lab1/Program.cs
+++ Multithreaded-and-distributed-programming/lab1/Program.cs
@@ -1,1 +1,24 @@
-﻿string regExp = @"x=([0-9]+);[^;]+;";
+﻿// string regExp = @"x=([0-9]+);[^;]+;";
+using System.Text.RegularExpressions;
+
+class Program
+{
+    private static int x;
+
+    static void Main()
+    {
+        int[] arr1 = { 0, 1, 2, 3, 5, 7, 10 };
+
+        string input = string.Join(" ", arr1);
+
+        string regExp = @"\b(0|1|2|3|5|7)\b";
+        // string regExp = @"x=([0-9]+);[^;]+;";
+
+        MatchCollection matches = Regex.Matches(input, regExp);
+
+        foreach (Match match in matches.Cast<Match>())
+        {
+            Console.WriteLine("Знайдено: " + match.Value);
+        }
+    }
+}
