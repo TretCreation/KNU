34a925f061bedccc700b7d5c9458ae8230607701
Volodymyr <tret.creation@gmail.com>
2023-10-12 18:32:26 +0300

    add: lab2 [MDP]

--- Multithreaded-and-distributed-programming/lab2/Program.cs
+++ Multithreaded-and-distributed-programming/lab2/Program.cs
@@ -1,2 +1,79 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+//TODO: FIX deadlocks
+public class DiningPhilosophers
+{
+    private const int NumPhilosophers = 5;
+    private IntPtr[] forks = new IntPtr[NumPhilosophers];
+
+    [DllImport("libc", SetLastError = true)]
+    private static extern int pthread_mutex_init(out IntPtr mutex, IntPtr attr);
+
+    [DllImport("libc", SetLastError = true)]
+    private static extern int pthread_mutex_lock(IntPtr mutex);
+
+    [DllImport("libc", SetLastError = true)]
+    private static extern int pthread_mutex_unlock(IntPtr mutex);
+
+    public DiningPhilosophers()
+    {
+        for (int i = 0; i < NumPhilosophers; i++)
+        {
+            pthread_mutex_init(out forks[i], IntPtr.Zero);
+        }
+    }
+
+    public void StartDining()
+    {
+        Thread[] philosophers = new Thread[NumPhilosophers];
+
+        for (int i = 0; i < NumPhilosophers; i++)
+        {
+            int philosopherId = i;
+            philosophers[i] = new Thread(() => Philosopher(philosopherId));
+            philosophers[i].Start();
+        }
+
+        for (int i = 0; i < NumPhilosophers; i++)
+        {
+            philosophers[i].Join();
+        }
+    }
+
+    private void Philosopher(int philosopherId)
+    {
+        int leftFork = philosopherId;
+        int rightFork = (philosopherId + 1) % NumPhilosophers;
+
+        while (true)
+        {
+            Console.WriteLine($"Philosopher {philosopherId} is thinking.");
+
+            Thread.Sleep(TimeSpan.FromSeconds(new Random().Next(3))); // Thinking
+
+            Console.WriteLine($"Philosopher {philosopherId} is hungry and tries to pick up forks.");
+
+            if (pthread_mutex_trylock(forks[leftFork]) == 0)
+            {
+                if (pthread_mutex_trylock(forks[rightFork]) == 0)
+                {
+                    Console.WriteLine($"Philosopher {philosopherId} is eating.");
+                    Thread.Sleep(TimeSpan.FromSeconds(new Random().Next(3))); // Eating
+                    pthread_mutex_unlock(forks[rightFork]);
+                }
+                pthread_mutex_unlock(forks[leftFork]);
+            }
+        }
+    }
+
+    public static void Main(string[] args)
+    {
+        DiningPhilosophers diningPhilosophers = new DiningPhilosophers();
+        diningPhilosophers.StartDining();
+    }
+
+    [DllImport("libc", SetLastError = true)]
+    private static extern int pthread_mutex_trylock(IntPtr mutex);
+}
