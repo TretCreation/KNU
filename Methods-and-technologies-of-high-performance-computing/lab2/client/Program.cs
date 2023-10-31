98cb787f4cf88b579d51078f76660d17259c82e2
Volodymyr <tret.creation@gmail.com>
2023-10-31 16:43:01 +0200

    add: lab2 [MTHPC]

---
+++ Methods-and-technologies-of-high-performance-computing/lab2/client/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ServiceModel;
+
+class Client
+{
+    static void Main()
+    {
+        ChannelFactory<IRemoteService> factory = new ChannelFactory<IRemoteService>(
+            new WSHttpBinding(),
+            "http://localhost:1234/RemoteService"
+        );
+        IRemoteService proxy = factory.CreateChannel();
+
+        string result = proxy.DoSomething();
+        Console.WriteLine("Результат: " + result);
+
+        ((IClientChannel)proxy).Close();
+    }
+}
